d849573c949a302068acdbf035b6f5f9152c6b27
chrisjones4 <chrisjones4@u.boisestate.edu>
2023-03-01 10:24:42 -0700

    clean up init file, copy over mol2 and xml files

--- src/init.py
+++ src/init.py
@@ -23,36 +23,22 @@ def get_parameters():
     parameters = OrderedDict()
 
     ### SYSTEM GENERATION PARAMETERS ###
-    parameters["molecule"] = ["PPS"]
-    parameters["molecule_kwargs"] = [None]
-    parameters["system_seed"] = [24]
-    parameters["system_type"] = ["Pack"]
-    parameters["density"] = [1.35]
+    parameters["density"] = [0.8]
     parameters["n_compounds"] = [25]
     parameters["system_kwargs"] = [None]
-    parameters["forcefield"] = ["OPLS_AA_PPS"]
     parameters["remove_hydrogens"] = [True]
 
-    ### SIM FROM RESTART PARAMETERS ###
-    parameters["restart_file"] = [None]
-
     ### SIMULATION PARAMETERS ###
-    parameters["methods"] = ["run_update_volume", "run_NVT"]
     parameters["tau_kt"] = [0.1]
-    parameters["tau_p"] = [None]
-    parameters["pressure"] = [None]
     parameters["dt"] = [0.0003]
     parameters["r_cut"] = [2.5]
     parameters["sim_seed"] = [42]
-    parameters["neighbor_list"] = ["Cell"]
-    parameters["walls"] = [None]
-    parameters["init_shrink_kT"] = [7]
-    parameters["final_shrink_kT"] = [7]
     parameters["shrink_steps"] = [1e6]
     parameters["shrink_period"] = [1]
+    parameters["shrink_kT"] = [3.0]
     ### Quench related parameters ###
-    parameters["kT_quench"] = [7]
-    parameters["n_steps"] = [2e5]
+    parameters["kT"] = [2.0]
+    parameters["n_steps"] = [5e6]
 
 def main():
     project = signac.init_project() # Set the signac project name
